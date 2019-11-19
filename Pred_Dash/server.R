1e5980ac04b58f4d712f0ccafefd557e9bf11783
BouzidiImen <Bouzidi.imen@outlook.com>
2019-11-19 01:07:15 +0100

    added Code

--- Pred_Dash/server.R
+++ Pred_Dash/server.R
@@ -1,26 +1,168 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(plotly)
+library(shinydashboard)
+library(DT)
+library(plotly)
+library(caret)
+library(dygraphs)
+library(xts)         
+library(tidyverse)
+library(lubridate)
+library(forecast)
+library(ggplot2)
+library(plotly)
+library(corrplot)
+library(RColorBrewer)
+d=read_excel('donnees_Electricite.xlsx',sheet = 2)
+Date=paste(d$annee,d$mois,d$jour,sep = '-')
+d$Date=as.Date(Date)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
-
+            
+output$table = DT::renderDataTable({
+  
+    DT::datatable(d,options = list(scrollX = TRUE))
+    
 })
+output$sum <-renderPrint({
+  f=d
+  for ( i in 9:29) { f[[i]] <- as.factor (f[[i]]) }
+  summary(f)
+})          
+
+output$tb<- renderUI({
+    tabsetPanel(
+      
+      tabPanel("Dataset",br(), DT::dataTableOutput("table")),
+      tabPanel("Summary",br(),verbatimTextOutput("sum"))) })            
+
+output$plot=renderDygraph({
+  
+  Variab<- ts(d[[input$Var]],start = c(2010, as.numeric(format(d$Date[1], "%j"))),
+              frequency = 365)
+  
+  don <- xts(x =Variab, order.by = d$Date)
+  
+  dyg=dygraph(don) %>%
+    dyOptions(labelsUTC = TRUE, fillGraph=TRUE, fillAlpha=0.1, drawGrid = FALSE, colors="#D8AE5A") %>%
+    dyRangeSelector() %>%
+    dyCrosshair(direction = "vertical") %>%
+    dyHighlight(highlightCircleSize = 5, highlightSeriesBackgroundAlpha = 0.2, hideOnMouseOut = FALSE)  %>%
+    dyRoller(rollPeriod = 1)
+  dyg
+  
+})
+
+output$plot2=renderPlot({
+  
+  Variab<- ts(d[[input$Var]],start = c(2010, as.numeric(format(d$Date[1], "%j"))),
+              frequency = 365)
+  decompose(Variab) %>% autoplot+theme_bw()
+  
+})
+
+output$plot3=renderPlotly({
+  
+ p= plot_ly(x = d[[input$Var3]], y = d[[8]],type = 'scatter', 
+            mode   = 'markers',
+               marker = list(size = 10,
+                             color = '#80bfff',#ccccff
+                             line = list(color = '#3399ff',
+                                         width = 2))) %>%
+    layout(title = 'Styled Scatter',
+           yaxis = list(zeroline = FALSE),
+           xaxis = list(zeroline = FALSE))
+  
+  p
+  
+  
+})
+
+output$down<- downloadHandler(
+  filename =  function() {
+    paste("Indkmeans.pdf")
+  },
+  # content is a function with argument file. content writes the plot to the device
+  content = function(file) {
+    pdf(file) # open the pdf device
+    print(
+      plot_ly(x = d[[input$Var3]], y = d[[8]],type = 'scatter', 
+              mode   = 'markers',
+              marker = list(size = 10,
+                            color = '#80bfff',#ccccff
+                            line = list(color = '#3399ff',
+                                        width = 2))) %>%
+        layout(title = 'Styled Scatter',
+               yaxis = list(zeroline = FALSE),
+               xaxis = list(zeroline = FALSE))
+    )
+    dev.off()  # turn the device off
+  }
+)
+
+
+
+output$ttt= renderPlot({
+  
+  M=cor(d[,5:8])
+  corrplot(M, type="upper", method='pie')
+  
+}) 
+
+
+
+output$text=renderPrint({
+  
+  
+  formule = paste("Energie_trans",'~',paste(c(input$Var4,input$Var5), collapse= "+"))
+  a = unlist(input$nb)
+  a = unlist(input$nb)
+  b=2922-a
+  train <- d[1:b, ] 
+  train=ts(train,start = 2010,frequency = 365)
+ fit <- tslm(formula(formule), data = train)
+  summary(fit)
+})
+output$plot4=renderPlotly({
+  formule = paste("Energie_trans",'~',paste(c(input$Var4,input$Var5), collapse= "+"))
+  a = unlist(input$nb)
+  b=2922-a
+  train1 <- d[1:b, ] #excluding the last two years
+  test <-  d[b+1:2922, ]
+  
+  train=ts(train1,start = 2010, frequency = 365)
+   fit <- tslm(formula(formule), data = train)
+  Energie_trans<-train1$Energie_trans
+  
+  fore<-forecast(fit,test)
+  plot_ly() %>%
+    add_lines(x = (d)$Date , y = (d)$Energie_trans,
+              color = I('#595959') , name = "observed") %>%
+    add_ribbons(x = (test)$Date, ymin = fore$lower[, 2], ymax = fore$upper[, 2],
+                color = I("#b3b3ff"), name = "95% confidence") %>%
+    add_lines(x = (test)$Date, y = fore$mean, color = I("#0073e6"), name = "prediction")
+  
+  
+  
+  
+})
+output$text2=renderPrint({
+  
+  
+  formule = paste("Energie_trans",'~',paste(c(input$Var4,input$Var5), collapse= "+"))
+  a = unlist(input$nb)
+  b=2922-a
+  train <- d[1:b, ] #excluding the last two years
+  train=ts(train,start = 2010,frequency = 365)
+  fit <- tslm(formula(formule), data = train)
+  accuracy(fit)
+})
+
+
+
+
+ })
+
+
+
+    
--- Pred_Dash/ui.R
+++ Pred_Dash/ui.R
@@ -1,33 +1,170 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(plotly)
+library(shinydashboard)
+library(DT)
+library(plotly)
+library(caret)
+library(dygraphs)
+library(xts)         
+library(tidyverse)
+library(lubridate)
+library(forecast)
+library(ggplot2)
+library(plotly)
+library(readxl)
+d=read_excel('donnees_Electricite.xlsx',sheet = 2)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+shinyUI(dashboardPage(
+    
+    
+    dashboardHeader(title = 'Electrical Energy'),
+    ########## sider panel ####
+    dashboardSidebar( width = 250,
+                      sidebarMenu(
+                          menuItem("Visualize Data", tabName = "VD", icon = icon("boxes")),
+                          
+                          menuItem("Exploratory Analysis", tabName = "DA", icon = icon("connectdevelop"),
+                                   startExpanded = T,
+                                   menuSubItem("Univariate Analysis", tabName = "UA", icon = icon("boxes")),
+                                   menuSubItem("Bivariate analysis", tabName = "BA", icon = icon("megaport"))
+                          ),
+                          menuItem("Prediction", tabName = "ML", icon = icon("slideshare"))
+                          
+                          
+                          
+                      )),
+    dashboardBody(
+        tabItems(
+            
+        #####Vizualise Data #########
+            
+        tabItem(
+            tabName = 'VD', uiOutput('tb'),
+            
+            tags$div(id="cite",
+                             '\U00A9 Imen Bouzidi'
+            )
+            
+            #tabPanel("Summary",br(), verbatimTextOutput("sum"))
+            
         ),
+        
+        
+            ########## Univariate analysis ######
+            tabItem(  tabName = "UA",
+                      
+                      box( status = "primary",  width = 12,
+                                      tabsetPanel(
+                                          selected = "Plot",
+                                          br(),
+                                         
+                                              selectInput("Var", "Choose a variable :",width = '200px',
+                                                          colnames(d[,5:8]),selected = "Energie_trans"),
+                                          
+                                        
+                            
+                                          tabPanel("Plot",br(),dygraphOutput("plot"),downloadButton(outputId = "down", label = "Download the plot")),
+                                          tabPanel("Decomposition",br(),plotOutput('plot2'))
+                                      )),
+                                 tags$head(tags$style(" #cite {
+                                      color: #949793;
+                                      position: absolute;
+                                      bottom: 10px;
+                                      right: 10px;
+                                      font-size: 12px;
+                                      }
+                                      ")),
+                      
+                      tags$div(id="cite",
+                               '\U00A9 Imen Bouzidi'
+                      )),
+            ########## Bivariate analysis ########
+            tabItem(
+                tabName = "BA",
+                
+                fluidRow( 
+                box( status = "primary",  width = 8 ,
+                     tabsetPanel(
+                         
+                         selected = "Plot",
+                         
+                         
+                         tabPanel("Plot",
+                                  br(),
+                                  
+                                  selectInput("Var3", "Choose a variable :",width = '200px',
+                                              colnames(d[,5:7]),selected = "tmin"),
+                                  
+                                  
+                                  br(),plotlyOutput('plot3'),br(),
+                                  downloadButton(outputId = "down", label = "Download the plot")),
+                         tabPanel("Correlation",br(),
+                                  br(),
+                                  
+                                  plotOutput('ttt'))
+                )
+                
+                
+               ),
+               tags$div(id="cite",
+                        '\U00A9 Imen Bouzidi'
+               )
+                
+                
+            )),
+            ########## Machine learning#####################
+            tabItem(tabName = "ML",
+                    fluidRow(  box( title = "Choose the variables and the method: ", solidHeader = T,status = "primary",  width = 4 , 
+                                    selectInput("Var4", "Choose model's features:",multiple = T
+                                                ,choices =names(d[,c(1,9:29)]),selected =names(d[,c(1,9:29)]) ),
+                                    
+                                    selectInput("Var5", "Choose which temperature to iclude:",multiple = F
+                                                ,choices =names(d[,5:7]),selected ='Tmoy'),
+                                    
+                                    
+                                    sliderInput("nb", "Number of days of prediction:",
+                                                 min = 1, max =365 , value = 365)
+                                    
+                    ),
+                    box(status = "primary",  width = 8,
+                        tabsetPanel(
+                            
+                            selected = "Plot",
+                            
+                            
+                            tabPanel("Prediction Plot",plotlyOutput('plot4')),
+                            tabPanel("Summary of fit",br(),verbatimTextOutput('text')),
+                            
+                            tabPanel("Accuracy",br(),verbatimTextOutput('text2'))
+                            
+                        )
+                    )
+                    
+                    
+                    ),
+                    tags$div(id="cite",
+                             '\U00A9 Imen Bouzidi'
+                    )
+                    
+                    
+                    
+            )
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+)
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
+
+)
 ))
+
+
+
